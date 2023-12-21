cef04a2a60810e41b05b60897ce536cde44bc329
Frank Wang <yuhsianw@gmail.com>
2023-12-21 12:52:55 -0500

    feat: setup typeorm configuration

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin_dashboard_user',
+      password: 'password',
+      database: 'admin_dashboard',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
