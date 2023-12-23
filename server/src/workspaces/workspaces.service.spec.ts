c065388dff3d1d54dde4c1ef6a4c99ffc20df6a3
Frank Wang <yuhsianw@gmail.com>
2023-12-23 13:05:07 -0500

    feat: add crud operations for workspace service

--- server/src/workspaces/workspaces.service.spec.ts
+++ server/src/workspaces/workspaces.service.spec.ts
@@ -1,18 +1,129 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WorkspacesService } from './workspaces.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Workspace } from './workspaces.entity';
+import { Repository } from 'typeorm';
 
 describe('WorkspacesService', () => {
   let service: WorkspacesService;
+  let repository: Repository<Workspace>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WorkspacesService],
+      providers: [
+        WorkspacesService,
+        { provide: getRepositoryToken(Workspace), useClass: Repository },
+      ],
     }).compile();
 
     service = module.get<WorkspacesService>(WorkspacesService);
+    repository = module.get<Repository<Workspace>>(
+      getRepositoryToken(Workspace),
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a new workspace', async () => {
+      const workspace: Workspace = {
+        id: 1,
+        name: 'Test Workspace',
+        domain: 'test',
+        members: [],
+        samlEnabled: false,
+      };
+      jest.spyOn(repository, 'save').mockResolvedValue(workspace);
+
+      const result = await service.create(workspace);
+
+      expect(repository.save).toHaveBeenCalledWith(workspace);
+      expect(result).toEqual(workspace);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should retrieve all workspaces', async () => {
+      const workspaces: Workspace[] = [
+        {
+          id: 1,
+          name: 'Workspace 1',
+          domain: 'workspace1',
+          members: [],
+          samlEnabled: false,
+        },
+        {
+          id: 2,
+          name: 'Workspace 2',
+          domain: 'workspace2',
+          members: [],
+          samlEnabled: true,
+        },
+      ];
+      jest.spyOn(repository, 'find').mockResolvedValue(workspaces);
+
+      const result = await service.findAll();
+
+      expect(repository.find).toHaveBeenCalled();
+      expect(result).toEqual(workspaces);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should retrieve a workspace by its ID', async () => {
+      const workspace: Workspace = {
+        id: 1,
+        name: 'Test Workspace',
+        domain: 'test',
+        members: [],
+        samlEnabled: false,
+      };
+      jest.spyOn(repository, 'findOne').mockResolvedValue(workspace);
+
+      const result = await service.findOne(1);
+
+      expect(repository.findOne).toHaveBeenCalledWith({ id: 1 });
+      expect(result).toEqual(workspace);
+    });
+
+    it('should return null if workspace is not found', async () => {
+      jest.spyOn(repository, 'findOne').mockResolvedValue(null);
+
+      const result = await service.findOne(1);
+
+      expect(repository.findOne).toHaveBeenCalledWith({ id: 1 });
+      expect(result).toBeNull();
+    });
+  });
+
+  describe('update', () => {
+    it('should update a workspace', async () => {
+      const workspace: Workspace = {
+        id: 1,
+        name: 'Test Workspace',
+        domain: 'test',
+        members: [],
+        samlEnabled: false,
+      };
+      jest.spyOn(repository, 'save').mockResolvedValue(workspace);
+
+      const result = await service.update(workspace);
+
+      expect(repository.save).toHaveBeenCalledWith(workspace);
+      expect(result).toEqual(workspace);
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove a workspace by its ID', async () => {
+      const id = 1;
+      jest.spyOn(repository, 'delete').mockResolvedValue(undefined);
+
+      await service.remove(id);
+
+      expect(repository.delete).toHaveBeenCalledWith(id);
+    });
   });
 });
--- server/src/workspaces/workspaces.service.ts
+++ server/src/workspaces/workspaces.service.ts
@@ -1,4 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Workspace } from './workspaces.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class WorkspacesService {}
+export class WorkspacesService {
+  constructor(
+    @InjectRepository(Workspace)
+    private workspacesRepository: Repository<Workspace>,
+  ) {}
+
+  /**
+   * Creates a new workspace.
+   * @param workspace - The workspace object to be created.
+   * @returns A promise that resolves to the created workspace.
+   */
+  create(workspace: Workspace): Promise<Workspace> {
+    return this.workspacesRepository.save(workspace);
+  }
+
+  /**
+   * Retrieves all workspaces.
+   * @returns A promise that resolves to an array of workspaces.
+   */
+  findAll(): Promise<Workspace[]> {
+    return this.workspacesRepository.find();
+  }
+
+  /**
+   * Retrieves a workspace by its ID.
+   * @param id - The ID of the workspace to retrieve.
+   * @returns A promise that resolves to the retrieved workspace, or null if not found.
+   */
+  findOne(id: number): Promise<Workspace | null> {
+    return this.workspacesRepository.findOneBy({ id });
+  }
+
+  /**
+   * Updates a workspace.
+   * @param workspace - The workspace object to be updated.
+   * @returns A promise that resolves to the updated workspace.
+   */
+  update(workspace: Workspace): Promise<Workspace> {
+    return this.workspacesRepository.save(workspace);
+  }
+
+  /**
+   * Removes a workspace by its ID.
+   * @param id - The ID of the workspace to remove.
+   * @returns A promise that resolves when the workspace is successfully removed.
+   */
+  async remove(id: number): Promise<void> {
+    await this.workspacesRepository.delete(id);
+  }
+}
