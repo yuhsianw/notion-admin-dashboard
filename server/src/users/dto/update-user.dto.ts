e62d970208d01465760d060d6729cb38a9b3c4c0
Frank Wang <yuhsianw@gmail.com>
2023-12-24 09:21:10 -0500

    feat: add create and update user dtos

---
+++ server/src/users/dto/update-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsOptional, IsEmail, IsNotEmpty } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsNotEmpty()
+  firstName?: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  lastName?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+}
