4b4c6c4afaa526e4229e2db2afc40d12b1e55c5a
Frank Wang <yuhsianw@gmail.com>
2023-12-26 00:23:14 -0500

    feat: add data grid component

--- client/src/components/Users/UsersLayout.tsx
+++ client/src/components/Users/UsersLayout.tsx
@@ -1,5 +1,24 @@
+import { Typography } from '@mui/material';
+import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
 import React from 'react';
 
 export default function UsersLayout() {
-  return <div>Users</div>;
+  const rows: GridRowsProp = [
+    { id: 1, col1: 'Hello', col2: 'World' },
+    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+    { id: 3, col1: 'MUI', col2: 'is Amazing' },
+  ];
+  const columns: GridColDef[] = [
+    { field: 'col1', headerName: 'Column 1', width: 150 },
+    { field: 'col2', headerName: 'Column 2', width: 150 },
+  ];
+
+  return (
+    <>
+      <Typography variant="h6" component="h1" gutterBottom>
+        Users
+      </Typography>
+      <DataGrid rows={rows} columns={columns} />
+    </>
+  );
 }
