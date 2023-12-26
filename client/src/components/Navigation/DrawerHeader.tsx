480a8f033475439eb84df29a61f9ea5daec5b0a1
Frank Wang <yuhsianw@gmail.com>
2023-12-25 22:30:44 -0500

    refactor: move styled components outside of react components

--- client/src/components/Navigation/DrawerHeader.tsx
+++ client/src/components/Navigation/DrawerHeader.tsx
@@ -1,14 +1,14 @@
 import { styled } from '@mui/material/styles';
 import React, { PropsWithChildren } from 'react';
 
-export default function DrawerHeader({ children }: PropsWithChildren<{}>) {
-  const StyledComponent = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    justifyContent: 'flex-end',
-    ...theme.mixins.toolbar, // necessary for content to be below app bar
-  }));
+const DrawerHeaderStyledComponent = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  justifyContent: 'flex-end',
+  ...theme.mixins.toolbar, // necessary for content to be below app bar
+}));
 
-  return <StyledComponent>{children}</StyledComponent>;
+export default function DrawerHeader({ children }: PropsWithChildren<{}>) {
+  return <DrawerHeaderStyledComponent>{children}</DrawerHeaderStyledComponent>;
 }
--- client/src/components/Navigation/Main.tsx
+++ client/src/components/Navigation/Main.tsx
@@ -3,38 +3,38 @@ import DrawerHeader from './DrawerHeader';
 import { styled } from '@mui/material';
 import { drawerWidth } from '../../config/constants';
 
+const MainStyledComponent = styled('main', {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<{
+  open?: boolean;
+}>(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  marginLeft: `-${drawerWidth}px`,
+  transition: theme.transitions.create('margin', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: 0,
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
 interface MainProps {
   open: boolean;
 }
 
 export default function Main({ open, children }: PropsWithChildren<MainProps>) {
-  const StyledComponent = styled('main', {
-    shouldForwardProp: (prop) => prop !== 'open',
-  })<{
-    open?: boolean;
-  }>(({ theme, open }) => ({
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    marginLeft: `-${drawerWidth}px`,
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    ...(open && {
-      marginLeft: 0,
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    }),
-  }));
-
   return (
     <>
-      <StyledComponent open={open}>
+      <MainStyledComponent open={open}>
         <DrawerHeader />
         {children}
-      </StyledComponent>
+      </MainStyledComponent>
     </>
   );
 }
