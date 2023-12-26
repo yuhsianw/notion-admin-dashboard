9c1a11545bb36fcc5d821a441aa2312840c6eadc
Frank Wang <yuhsianw@gmail.com>
2023-12-26 09:39:01 -0500

    feat: add home page

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,12 +3,14 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import WorkspacesLayout from './Workspaces/WorkspacesLayout';
 import UsersLayout from './Users/UsersLayout';
 import AppLayout from './AppLayout';
+import HomePage from './HomePage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
     children: [
+      { path: '/', element: <HomePage /> },
       { path: 'workspaces', element: <WorkspacesLayout /> },
       { path: 'users', element: <UsersLayout /> },
     ],
---
+++ client/src/components/HomePage.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <Typography variant="h4" component="h1" gutterBottom>
+      Welcome to the Notion Admin Dashboard!
+      {/* TODO:  Add instruction */}
+    </Typography>
+  );
+}
