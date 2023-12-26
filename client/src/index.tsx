e3236084b018441fc11fae5d8c822ac5e0963602
Frank Wang <yuhsianw@gmail.com>
2023-12-25 20:09:26 -0500

    refactor: move files to utils and components dir

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
