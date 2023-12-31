0ead8f9019530d65789b0db01ada4d1bfa038f63
Frank Wang <yuhsianw@gmail.com>
2023-12-31 13:04:06 -0500

    feat(server): add membership controls in user service

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UserWorkspaceModule } from 'src/user-workspace/user-workspace.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), UserWorkspaceModule],
   providers: [UsersService],
   controllers: [UsersController],
 })
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -4,6 +4,12 @@ import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserWorkspaceService } from 'src/user-workspace/user-workspace.service';
+
+interface WorkspaceChangeLists {
+  addedWorkspaceIds: string[];
+  removedWorkspaceIds: string[];
+}
 
 /**
  * Service class for managing users in the database.
@@ -16,15 +22,71 @@ export class UsersService {
      */
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+
+    private userWorkspaceService: UserWorkspaceService,
   ) {}
 
   /**
+   * Get the list of workspaces that were added and removed.
+   * @param oldWorkspaceIds - The old list of workspace IDs.
+   * @param newWorkspaceIds - The new list of workspace IDs.
+   * @returns An object containing the added and removed workspace IDs.
+   */
+  private getWorkspaceChangeLists(
+    oldWorkspaceIds: string[],
+    newWorkspaceIds: string[],
+  ): WorkspaceChangeLists {
+    const removedSet = new Set(oldWorkspaceIds);
+    const addedSet = new Set<string>();
+    newWorkspaceIds.forEach((id) => {
+      if (removedSet.has(id)) {
+        removedSet.delete(id);
+      } else {
+        addedSet.add(id);
+      }
+    });
+    return {
+      addedWorkspaceIds: Array.from(addedSet),
+      removedWorkspaceIds: Array.from(removedSet),
+    };
+  }
+
+  /**
+   * Update the user's memberships.
+   * @param userId - The ID of the user.
+   * @param newWorkspaceIds - The new list of workspace IDs.
+   * @returns A promise that resolves when the memberships are updated.
+   */
+  private async updateMemberships(
+    userId: string,
+    newWorkspaceIds: string[],
+  ): Promise<void> {
+    const oldWorkspaceIds = (
+      await this.userWorkspaceService.findMembershipsByUserId(userId)
+    ).map((membership) => membership.workspaceId);
+    const { addedWorkspaceIds, removedWorkspaceIds } =
+      this.getWorkspaceChangeLists(oldWorkspaceIds, newWorkspaceIds);
+    await this.userWorkspaceService.createMemberships(addedWorkspaceIds, [
+      userId,
+    ]);
+    await this.userWorkspaceService.removeMemberships(removedWorkspaceIds, [
+      userId,
+    ]);
+  }
+
+  /**
    * Create a new user.
-   * @param user - The user data.
+   * @param userDto - The user data.
    * @returns The created user.
    */
-  create(user: CreateUserDto) {
-    return this.usersRepository.save(user);
+  async create(userDto: CreateUserDto) {
+    const newUser = await this.usersRepository.save(
+      Object.assign(new User(), userDto),
+    );
+    if (userDto.workspaces !== undefined) {
+      await this.updateMemberships(newUser.id, userDto.workspaces);
+    }
+    return newUser;
   }
 
   /**
@@ -65,6 +127,9 @@ export class UsersService {
     if (updateUserDto.email !== undefined) {
       user.email = updateUserDto.email;
     }
+    if (updateUserDto.workspaces !== undefined) {
+      await this.updateMemberships(user.id, updateUserDto.workspaces);
+    }
 
     // Save the updated user to the database
     return this.usersRepository.save(user);
@@ -76,6 +141,7 @@ export class UsersService {
    * @returns A promise that resolves when the user is removed.
    */
   async remove(id: string): Promise<void> {
+    await this.updateMemberships(id, []);
     await this.usersRepository.delete(id);
   }
 }
