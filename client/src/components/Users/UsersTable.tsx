91ef092eff0689685254c70cf37bec4fdaa13c6a
Frank Wang <yuhsianw@gmail.com>
2023-12-31 20:59:17 -0500

    feat(client): convert workspaces column to multiple select

--- client/src/components/Users/UsersTable.tsx
+++ client/src/components/Users/UsersTable.tsx
@@ -7,9 +7,14 @@ import {
   updateUser,
 } from '../../services/userService';
 import Table from '../Table/Table';
-import { GetUserDto } from '../../dto/get-user.dto';
 import { DEFAULT_USER_ROWS, POLLING_INTERVAL } from '../../config/constants';
 import { renderEmail } from '@mui/x-data-grid-generator';
+import MultipleSelectEditCell, {
+  MultipleSelectEditCellOption,
+} from '../Table/MultipleSelectEditCell';
+import workspaceService from '../../services/workspaceService';
+import GetWorkspaceDto from '../../dto/get-workspace.dto';
+import { GetUserDto } from '../../dto/get-user.dto';
 
 export interface UserGridRow {
   id: string;
@@ -26,6 +31,9 @@ export default function UserTable() {
    * will allow the pollingInterval to access the latest rows.
    */
   const rowsRef = useRef(rows);
+  const [workspaceOptions, setWorkspaceOptions] = useState<
+    MultipleSelectEditCellOption[]
+  >([]);
 
   /**
    * TODO:  Add column validation https://mui.com/x/react-data-grid/filtering/quick-filter/#custom-filtering-logic
@@ -48,24 +56,35 @@ export default function UserTable() {
       editable: true,
       renderCell: renderEmail,
     },
-    /**
-     * TODO:  Multiple select for workspaces
-     * @see: single select example
-     * - https://mui.com/x/react-data-grid/filtering/quick-filter/#custom-filtering-logic
-     * - https://github.com/mui/mui-x/blob/a769a9164c233116a7186e4e65d56d83003c0b6f/packages/grid/x-data-grid-generator/src/columns/employees.columns.tsx#L112C4-L112C4
-     * @see: multiple select example https://mui.com/material-ui/react-select/#chip
-     */
     {
       field: 'workspaces',
       headerName: 'Workspaces',
+      type: 'singleSelect',
       width: 220,
       editable: true,
-    },
+      sortable: false,
+      valueOptions: workspaceOptions,
+      valueFormatter: ({ value }) => {
+        return value
+          .map((workspaceId) => {
+            const workspace = workspaceOptions.find(
+              (workspace) => workspace.value === workspaceId,
+            );
+            return workspace ? workspace.label : '';
+          })
+          .join(', ');
+      },
+      renderEditCell: (params) => (
+        <MultipleSelectEditCell
+          options={workspaceOptions}
+          value={params.value}
+          {...params}></MultipleSelectEditCell>
+      ),
+      // TODO: Allow filtering by workspaces
+      filterable: false,
+      // filterOperators: []
+    } as GridColDef<any, GetWorkspaceDto['members'], any>,
   ];
-
-  useEffect(() => {
-    rowsRef.current = rows;
-  }, [rows]);
 
   const fetchRows = () => {
     getAllUsers()
@@ -88,6 +107,22 @@ export default function UserTable() {
         // TODO:  https://mui.com/material-ui/react-alert/
       });
   };
+
+  useEffect(() => {
+    workspaceService.getAllWorkspaces().then((data) => {
+      const options = data.map((workspace) => {
+        return {
+          value: workspace.id,
+          label: workspace.name,
+        };
+      });
+      setWorkspaceOptions(options);
+    });
+  }, []);
+
+  useEffect(() => {
+    rowsRef.current = rows;
+  }, [rows]);
 
   useEffect(() => {
     fetchRows();
