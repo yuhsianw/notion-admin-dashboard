c5fab2071d8588321c8cac66bdeb26592fe3b74d
Frank Wang <yuhsianw@gmail.com>
2024-01-02 15:09:56 -0500

    feat(server): setup server for heroku

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
-import { CORS_ORIGIN, PORT } from './config/constants';
+import { CORS_ORIGIN, DEFAULT_PORT } from './config/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors({ origin: CORS_ORIGIN }));
-  await app.listen(PORT);
+  /**
+   * Start the server and listen on the port in the environment variable PORT.
+   * @see: https://devcenter.heroku.com/articles/preparing-a-codebase-for-heroku-deployment#4-listen-on-the-correct-port
+   */
+  console.log(`Listening on port ${process.env.PORT ?? DEFAULT_PORT}`);
+  await app.listen(process.env.PORT ?? DEFAULT_PORT);
 }
 bootstrap();
